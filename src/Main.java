cedcc0e6e13c903c9d68bd8a34a9a62760405a2d
Ahmet <aserdarow05@gmail.com>
2024-12-17 16:41:34 +0300

    улучщения

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello World!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 2; i++) {
+
 
             System.out.println("i = " + i);
         }
