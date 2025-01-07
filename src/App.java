b01025495b7177711cc6adcd6dc3f0cd1cdc5772
Ahmet <aserdarow05@gmail.com>
2025-01-08 01:46:07 +0300

    улучщения

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        var king = "King Balon the 6th";
+
+        // BEGIN (write your solution here)
+        var newKing = 6;
+        var newLockRoom = 17;
+        System.out.println(king + " has " + (newKing * newLockRoom) + " rooms.");
+        // END
+    }
+}
