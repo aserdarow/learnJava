6dd0943274ff248d78602e4f414a517f8d106fee
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-01-08 14:12:55 +0000

    Практика
    - Добавлен шаблон для домашки

---
+++ src/tasks/chapter1/VariantA.java
@@ -0,0 +1,16 @@
+package tasks.chapter1;
+
+public class VariantA {
+
+    // TODO: 1
+    public void task1() {
+
+    }
+
+    // TODO: 2
+
+
+    public static void main(String[] args) {
+
+    }
+}
