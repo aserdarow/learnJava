36a16ac11b6e13d4dea094d9cb334c5b843da857
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-01-19 13:38:11 +0000

    Проверка ДЗ | Глава 1 - Вариант A
    - Правильны: №5
    - На доработку: №6

--- src/tasks/chapter1/VariantA.java
+++ src/tasks/chapter1/VariantA.java
@@ -13,7 +13,7 @@ public class VariantA {
         scan.close();
     }
 
-    public static void task2(){
+    public static void task2() {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter your age: ");
@@ -28,7 +28,7 @@ public class VariantA {
         scan.close();
     }
 
-    public static void task3(){
+    public static void task3() {
         Scanner scan = new Scanner(System.in);
 
         System.out.println(" Enter number of count: ");
@@ -41,15 +41,21 @@ public class VariantA {
             int num = scan.nextInt();
             numbers[i] = num;
         }
+        scan.close();
+
+//        System.out.println("Answer: ");
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.print(numbers[i] + " ");
+//        }
 
         System.out.println("Answer: ");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
-
+        for (int number : numbers) {
+            System.out.print(number + " ");
         }
+
     }
 
-    public static void task4(){
+    public static void task4() {
         String password = "ahmet";
         Scanner scan = new Scanner(System.in);
 
@@ -63,44 +69,37 @@ public class VariantA {
 
     }
 
-
-
-    public static void task5(){
-
+    public static void task5() {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter your of count: ");
-        int numYourCount = scan.nextInt();
+        System.out.println("Enter number of count: ");
+        int numOfCount = scan.nextInt();
+        int[] numbers = new int[numOfCount];
 
-        int[] number = new int[numYourCount];
-
-        for (int i = 0; i < numYourCount; i++) {
+        for (int i = 0; i < numOfCount; i++) {
             System.out.println("Enter your number: ");
             int num = scan.nextInt();
-            number[i] = num;
+            numbers[i] = num;
         }
+
+        scan.close();
 
         int sum = 0;
         int product = 1;
 
-        for (int i = 0; i < numYourCount; i++) {
-            sum += number[i];
-            product *= number[i];
+        for (int j = 0; j < numOfCount; j++) {
+            sum += numbers[j];
+            product *= numbers[j];
         }
 
         System.out.println("Answer: ");
-        System.out.println("Сумма: " + sum);
-        System.out.println("Произведение: " + product);
-
-        scan.close();
+        System.out.println("Sum of numbers: " + sum);
+        System.out.println("Multiple of numbers: " + product);
 
     }
-    // polucaem cislo kak v 3task zapisyvat massiw zatem nado passitat summy i umnazeniya(proyezvideniya)
-    // v itogy dolzjno byt summa i proizvedeniye vseh cisel
 
-
-
-    public static void task6(){
+    public static void task6() {
+        // https://www.geeksforgeeks.org/java-current-date-time/
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Твоя Фамилия: ");
@@ -119,17 +118,13 @@ public class VariantA {
 
         scan.close();
     }
-    // polucit 3 straky 1familiya 2data polucennennoga zadaniya 3data zdaci zadaniya
-
-
 
     public static void main(String[] args) {
-       // task1();
+//        task1();
 //        task2();
 //        task3();
 //        task4();
-        task5();
+//        task5();
 //        task6();
-
     }
 }
