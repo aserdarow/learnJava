82b577c7d62d43620e2dfef6b005f5861d41e088
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-01-29 13:17:28 +0000

    Обсуждение | Глава 1 - Вариант B
    - Простые числа
    - Счастливые числа
    - Алгоритмы сортировки
    
    - ДЗ: №2, №3, №5, №7, №9, №10 и №11

--- src/tasks/chapter1/VariantB.java
+++ src/tasks/chapter1/VariantB.java
@@ -3,18 +3,17 @@ package tasks.chapter1;
 import java.util.Scanner;
 
 public class VariantB {
-
-    public static int[] getNumbersFromInput() {
+    public static long[] getNumbersFromInput() {
         Scanner scan = new Scanner(System.in);
 
         System.out.println(" Enter number of count: ");
         int numOfCount = scan.nextInt();
 
-        int[] numbers = new int[numOfCount];
+        long[] numbers = new long[numOfCount];
 
         for (int i = 0; i < numOfCount; i++) {
             System.out.println("Enter your number: ");
-            int num = scan.nextInt();
+            long num = scan.nextLong();
             numbers[i] = num;
         }
         scan.close();
@@ -22,12 +21,12 @@ public class VariantB {
         return numbers;
     }
 
-    public static boolean isEven(int number) {
+    public static boolean isEven(long number) {
         return number % 2 == 0;
     }
 
-    public static void task1(int[] nums) {
-        for (int num : nums) {
+    public static void task1(long[] nums) {
+        for (long num : nums) {
             if (isEven(num)) {
                 System.out.println(num + " - четное");
             } else {
@@ -51,21 +50,72 @@ public class VariantB {
 
     // TODO: остальные задачи
 
-    public static void task4(int[] nums) {
-        for (int num : nums) {
+    public static void task4(long[] nums) {
+        for (long num : nums) {
             if (num % (5 * 7) == 0) {
                 System.out.println(num + " - делится на 5 и на 7");
             }
         }
     }
 
-    public static void main(String[] args) {
-        int[] numbers = getNumbersFromInput();
+    //9263567233
+//132351453426236
+    public static boolean isPrimeImprove(long num) {
+        long numRoot = (long) Math.sqrt(num) + 1;
 
-        task1(numbers);
+        for (long i = 2; i < numRoot; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static boolean isPrime(long num) {
+        for (long i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void task6(long[] nums) {
+        for (long num : nums) {
+            if (isPrimeImprove(num)) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    public static void task9(long[] nums) {
+        // https://thecode.media/is-happy/
+
+        // TODO: В программировании под счастливым числом понимают натуральное число,
+        //  сумма квадратов цифр которого в итоге равна 1
+    }
+
+    public static void task10(long[] nums) {
+        // TODO: Число палиндром, 1221, 12321
+    }
+
+    public static void task11(long nums) {
+        //    [1, 3, 5, 7, 11, 2, 0]
+    }
+
+
+    public static void main(String[] args) {
+        long[] numbers = getNumbersFromInput();
+
+//        task1(numbers);
 //        task2(numbers);
 //        task3(numbers);
-        task4(numbers);
+//        task4(numbers);
+        task6(numbers);
+
+        /*
+        List of prime numbers: http://compoasso.free.fr/primelistweb/page/prime/liste_online_en.php
+         */
     }
 }
-
