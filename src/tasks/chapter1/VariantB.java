5743b7bb8b034df0cb1668939a1a70dd42f7f886
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-02-05 06:31:27 +0000

    Обсуждение | Глава 1 - Вариант B
    - ДЗ: №7

--- src/tasks/chapter1/VariantB.java
+++ src/tasks/chapter1/VariantB.java
@@ -35,46 +35,96 @@ public class VariantB {
         }
     }
 
-//    public static void task2(long[] nums) {
-//        Scanner scan = new Scanner(System.in);
-//
-//        long max = Integer.MAX_VALUE;
-//        long min = Integer.MIN_VALUE;
-//
-//        for (long num : nums) {
-//            long numbers = scan.nextLong();
-//            if (numbers < min) {
-//                min = numbers;
-//            }
-//            if (numbers > max) {
-//                max = numbers;
-//            }
-//
-//        }
-//
-//        System.out.println("Наименьшее число: " + min);
-//        System.out.println("Наибольшее число: " + max);
-//
-//        scan.close();
-//
-//        // {-5, -20, -40, 1}
-//    }
+    public static void task2(long[] nums) {
+        long max = Integer.MIN_VALUE;
+        long min = Integer.MAX_VALUE;
 
+        // Approach 1
+        for (long num : nums) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
 
-    public static void  task3(long[] nums){
-        for (long num : nums){
-            if (num % 3 == 0){
+        // Approach 2
+//        long max = nums[0];
+//        long min = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            long num = nums[i];
+
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+
+        }
+
+        System.out.println("Наименьшее число: " + min);
+        System.out.println("Наибольшее число: " + max);
+
+    }
+
+    public static void task3(long[] nums) {
+        for (long num : nums) {
+            if (num % 3 == 0) {
                 System.out.println(num + " - делится на 3 и на 9");
             }
         }
     }
 
-    // TODO: остальные задачи
-
     public static void task4(long[] nums) {
         for (long num : nums) {
             if (num % (5 * 7) == 0) {
                 System.out.println(num + " - делится на 5 и на 7");
+            }
+        }
+    }
+
+    public static boolean isAllDigitsUnique(long n) {
+        long lastDigit = n % 10;
+        n = (int) (n / 10);
+
+        long middleDigit = n % 10;
+        n = (int) (n / 10);
+
+        long firstDigit = n % 10;
+        n = (int) (n / 10);
+
+
+//        System.out.printf("%d %d %d", firstDigit, middleDigit,  lastDigit);
+
+        return lastDigit != firstDigit && middleDigit != firstDigit && lastDigit != middleDigit;
+    }
+
+    public static void task5(long[] nums) {
+        // 1. Это трехзначное число?
+        // Да - то можно проверить его цифры.
+        // Нет - то переходить к следующему числу в массиве.
+
+        // 2. Как получить эти цифры числа?
+        // предыдущая цифра = 3
+        // текущая цифра =
+
+        // c 783 % 10 = 3
+//        783 / 10 = (long) 78.3
+        // b 78 % 10 = 8
+//        78.3 / 10 (long) 7.8 = 7
+        // a 7 % 10 = 7
+//        7 / 10 (long) 0.7 = 0
+
+        // с != a and b != a and c != b
+
+        for (long num : nums) {
+            if (num > 99 && num < 1000) {
+                if (isAllDigitsUnique(num)) {
+                    System.out.println(num);
+                }
             }
         }
     }
@@ -110,30 +160,32 @@ public class VariantB {
         }
     }
 
-    public static void task9(long[] nums) {
-        // https://thecode.media/is-happy/
-
-        // TODO: В программировании под счастливым числом понимают натуральное число,
-        //  сумма квадратов цифр которого в итоге равна 1
+    public static void task7Desc(long[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            // TODO:По убыванию
+        }
     }
 
-    public static void task10(long[] nums) {
-        // TODO: Число палиндром, 1221, 12321
+    public static void task7Asc(long[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            // TODO:По возрастанию
+        }
     }
 
-    public static void task11(long nums) {
-        //    [1, 3, 5, 7, 11, 2, 0]
-    }
-
+    //    nums.length - 1
+    //     n = 7
+    //    [5, 3, 7, 9, 1, 5, 3]  // n - 1 = 6
 
     public static void main(String[] args) {
         long[] numbers = getNumbersFromInput();
 
-        //task1(numbers);
-        //task2(numbers);
-  task3(numbers);
+//        task1(numbers);
+//        task2(numbers);
+//        task3(numbers);
 //        task4(numbers);
-        //task6(numbers);
+        task5(numbers);
+//        task6(numbers);
+//        task7(numbers);
 
         /*
         List of prime numbers: http://compoasso.free.fr/primelistweb/page/prime/liste_online_en.php
