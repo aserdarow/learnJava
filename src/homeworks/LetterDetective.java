7d2ff5c77ea5a7eda677e923a59d862f8e831765
Ahmet <aserdarow05@gmail.com>
2025-02-13 11:11:39 +0300

    Homework - не готов еще

--- src/homeworks/LetterDetective.java
+++ src/homeworks/LetterDetective.java
@@ -1,5 +1,24 @@
 package homeworks;
 
-public class LetterDetective {
+import java.util.Scanner;
 
-}
+public class LetterDetective {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Добро пожаловать в программу \"Буква - Детектива\"!");
+        System.out.println("Выберите алфавит: \n1.Латинский.\n2.Кириллица.");
+        System.out.println("Введите номер алфавита: ");
+        int num = scan.nextInt();
+        if (num > 2) {
+            System.out.println("\"Упс! Выбран неверный режим.\n" +
+                    "Попробуйте ещё раз...\"");
+        }
+        if (num == 1) {
+            System.out.println("Введите букву " + num + " языка(\"Буĸва должна быть в верхнем регистре\"): ");
+
+//        boolean letter = new Boolean();
+//        if (letter)
+            scan.close();
+        }
+    }
