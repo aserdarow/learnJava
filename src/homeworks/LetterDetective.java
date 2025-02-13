79dcdc96607b0536ad2dfbe9f936746aedb03b5e
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-02-13 08:32:33 +0000

    Домашнее задание
    - Буква-Детектив

--- src/homeworks/LetterDetective.java
+++ src/homeworks/LetterDetective.java
@@ -3,6 +3,20 @@ package homeworks;
 import java.util.Scanner;
 
 public class LetterDetective {
+    private final String engVowels = "AEIOUY";
+    private final String ruVowels = "АЕЁИОУЫЭЮЯ";
+
+    private static char getUserLetter(String alphabetMsg) {
+        System.out.println("Введите букву " + alphabetMsg + ": ");
+
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+
+
+        return text.toUpperCase().charAt(0);
+    }
+
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -10,15 +24,21 @@ public class LetterDetective {
         System.out.println("Выберите алфавит: \n1.Латинский.\n2.Кириллица.");
         System.out.println("Введите номер алфавита: ");
         int num = scan.nextInt();
-        if (num > 2) {
-            System.out.println("\"Упс! Выбран неверный режим.\n" +
-                    "Попробуйте ещё раз...\"");
+//        if  (num < 1 || num > 2) {
+        if (num != 1 && num != 2) {
+            System.out.println(
+                    "\"Упс! Выбран неверный режим.\n" +
+                            "Попробуйте ещё раз...\""
+            );
         }
+        scan.close();
+
+
         if (num == 1) {
-            System.out.println("Введите букву " + num + " языка(\"Буĸва должна быть в верхнем регистре\"): ");
+            char letter = getUserLetter("латинкого алфавита");
 
 //        boolean letter = new Boolean();
 //        if (letter)
-            scan.close();
         }
     }
+}
